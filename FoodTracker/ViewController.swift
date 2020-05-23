e8d921939fbc33273524b5ab213dc13f4fe5ae03
Rey Lim <rlimosenero@yahoo.com>
2020-05-23 18:33:53 +0800

    Connect UI to Code  - textfield.delegate = self then conform to protocol

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: PROPERTIES
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var mealText: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mealText.delegate = self
     }
+    
+    
+    //MARK: ACTIONS
+    @IBAction func buttonDidPressed(_ sender: UIButton) {
+        mealLabel.text = "Default Text"
+    }
+    
 
 
+}
+
+extension ViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabel.text = textField.text
+    }
 }
 
