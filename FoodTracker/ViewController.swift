adc492370de7c6384be429d703afc5783316c996
rlimosenero <rlimosenero@yahoo.com>
2020-05-23 16:02:34 +0800

    initial files

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Rey Limosenero on 23/5/20.
+//  Copyright © 2020 rlim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
